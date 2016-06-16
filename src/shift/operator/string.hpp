cac8272e1896c5ed83afed078940d44953e3bf52
michael <mikkael@users.noreply.github.com>
2016-06-16 07:13:18 +0200

    fix header

--- src/shift/operator/string.hpp
+++ src/shift/operator/string.hpp
@@ -1,3 +1,9 @@
+
+//          Copyright Michael Mehling 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef SHIFT_OPERATOR_STRING_
 #define SHIFT_OPERATOR_STRING_
 
